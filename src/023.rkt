c846e89929a019f56ac9e1b71c5bedee9ab8031c
Atharva Shukla <shukla.at@husky.neu.edu>
2020-03-01 00:12:40 -0500

    23

---
+++ src/023.rkt
@@ -0,0 +1,28 @@
+#lang htdp/bsl
+
+; Exercise 23.
+; ------------
+;
+; The first 1String in "hello world" is "h". How does the following function
+; compute this result?
+;
+;   (define (string-first s)
+;     (substring s 0 1))
+;
+; Use the stepper to confirm your ideas. 
+;
+; -----------------------------------------------------------------------------
+
+
+(define (string-first s)
+  (substring s 0 1))
+
+
+(string-first "hello-world")
+; => "h"
+
+(string-first "hello-world")
+; ==
+(substring "hello-world" 0 1)
+; ==
+; "h"
