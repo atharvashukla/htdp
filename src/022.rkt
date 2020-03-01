c35a2532c9bf50eba4045acb4cb9ca1e675b2b3b
Atharva Shukla <shukla.at@husky.neu.edu>
2020-03-01 00:12:36 -0500

    22

---
+++ src/022.rkt
@@ -0,0 +1,31 @@
+#lang htdp/bsl
+
+; Exercise 22.
+; ------------
+; Use DrRacket’s stepper on this program fragment:
+;
+;    (define (distance-to-origin x y)
+;      (sqrt (+ (sqr x) (sqr y))))
+;    (distance-to-origin 3 4)
+;
+; Does the explanation match your intuition?
+; -----------------------------------------------------------------------------
+
+(define (distance-to-origin x y)
+  (sqrt (+ (sqr x) (sqr y))))
+
+(distance-to-origin 3 4)
+; => 5
+
+
+(distance-to-origin 3 4)
+; ==
+(sqrt (+ (sqr 3) (sqr 4)))
+; ==
+(sqrt (+ 9 (sqr 4)))
+; ==
+(sqrt (+ 9 16))
+; ==
+(sqrt 25)
+; ==
+5
