d2844aa7b6a7567f612e1e8c00c6df29f6fafe4f
Atharva Shukla <shukla.at@husky.neu.edu>
2020-02-29 23:32:27 -0500

    del old ex [1-10]

--- src/10ex.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname 10ex) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Exercise 10.
-;; ------------
-
-;; Now relax, eat, sleep, and then tackle the next chapter.
-
-;; -----------------------------------------------------------------------------
