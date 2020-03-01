7bd32a4f20738c0979da01df188f9b120f8de57b
Atharva Shukla <shukla.at@husky.neu.edu>
2020-03-01 00:36:23 -0500

    add ex 32

---
+++ src/032.rkt
@@ -0,0 +1,27 @@
+#lang htdp/bsl
+
+; Exercise 32.
+; ------------
+;
+; Most people no longer use desktop computers just to run applications but also
+; employ cell phones, tablets, and their cars’ information control screen. Soon
+; people will use wearable computers in the form of intelligent glasses,
+; clothes, and sports gear. In the somewhat more distant future, people may
+; come with built-in bio computers that directly interact with body functions.
+; Think of ten different forms of events that software applications on such
+; computers will have to deal with. 
+;
+; -----------------------------------------------------------------------------
+
+; Different events software systems in bio-computers may have to deal with:
+; 
+; 1.  Eye movement
+; 2.  Brain signals
+; 3.  Hormoral fluctuations
+; 4.  Ambient sound
+; 5.  Temperature
+; 6.  Blood pressure
+; 7.  Human voices
+; 8.  Movement
+; 9.  Object recognition
+; 10. Facial expression/body language
