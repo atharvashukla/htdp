97ace567568170115d0d770805273d37ee50eb18
Atharva Shukla <shukla.at@husky.neu.edu>
2020-03-01 00:12:47 -0500

    25

---
+++ src/025.rkt
@@ -0,0 +1,31 @@
+#lang htdp/bsl
+
+; Exercise 25.
+; ------------
+;
+; Take a look at this attempt to solve exercise 17:
+;
+;    (define (image-classify img)
+;      (cond
+;        [(>= (image-height img) (image-width img)) "tall"]
+;        [(= (image-height img) (image-width img)) "square"]
+;        [(<= (image-height img) (image-width img)) "wide"]))
+;
+; Does stepping through an application suggest a fix?
+;
+; -----------------------------------------------------------------------------
+
+(require 2htdp/image)
+
+(define (image-classify img)
+  (cond
+    [(>= (image-height img) (image-width img)) "tall"]
+    [(= (image-height img) (image-width img)) "square"]
+    [(<= (image-height img) (image-width img)) "wide"]))
+
+
+(image-classify (square 20 "solid" "red"))
+
+; Yes, the `>=` condition in the first clause
+; should be `>` only, otherwise the dimensions of the
+; square would compute to "tall" instead of "square"
