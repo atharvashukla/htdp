34682834ad4c5bd8d1867d2377f7cdcfbc6ea9d6
Atharva Shukla <shukla.at@husky.neu.edu>
2020-03-01 00:00:20 -0500

    add hash lang line

--- src/004.rkt
+++ src/004.rkt
@@ -1,6 +1,5 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname 4ex) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#lang htdp/bsl
+
 ; Exercise 4.
 ; -----------
 ; Use the same setup as in exercise 3 to create an expression that deletes the
--- src/005.rkt
+++ src/005.rkt
@@ -1,7 +1,7 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname 5ex) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; Exercise 5.
+#lang htdp/bsl
+
+; Exercise 5.#lang htdp/bsl
+
 ; -----------
 ;
 ; Use the 2htdp/image library to create the image of a simple boat or tree. 
