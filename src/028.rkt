af7c983c4fe04ae979e217f1bb594816c0b0994e
Atharva Shukla <shukla.at@husky.neu.edu>
2020-03-10 04:40:42 -0400

    add 28

--- src/028.rkt
+++ src/028.rkt
@@ -1,5 +1,12 @@
 #lang htdp/bsl
 
+; Exercise 28.
+; ------------
+; Determine the potential profit for these ticket prices: $1, $2, $3, $4, and
+; $5. Which price maximizes the profit of the movie theater? Determine the best
+; ticket price to a dime.
+; ------------------------------------------------------------------------------
+
 ; Constant Definitions
 (define PERCENT-CHANGE  0.1)
 (define PEOPLE-CHANGE   15)
