6f78d6f913c84166036363bba063ea4dc5bef9c4
Atharva Shukla <shukla.at@husky.neu.edu>
2020-03-10 03:07:28 -0400

    remove redundant (string-length str) from substring

--- src/003.rkt
+++ src/003.rkt
@@ -24,11 +24,10 @@
 ;
 ; -----------------------------------------------------------------------------
 
-
 (define str "helloworld")
 (define i 5)
 
 (string-append (substring str 0 i)
                "_"
-               (substring str i (string-length str)))
+               (substring str i))
 
